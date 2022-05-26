3855f6d4d4e1a294101b8e4232e6a0a14b21b805
rlawogns <rlawogns0411@naver.com>
2022-05-26 23:07:39 +0900

    refactor : bluetooth on/off

--- Bluetoothdetector/app/src/main/java/com/example/bluetoothdetector/test_Bluetooth.kt
+++ Bluetoothdetector/app/src/main/java/com/example/bluetoothdetector/test_Bluetooth.kt
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -20,7 +21,7 @@ import kotlinx.android.synthetic.main.activity_test_bluetooth.*
 
 class test_Bluetooth : AppCompatActivity() {
     private val REQUEST_ENABLE_BT=1
-    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var mBluetoothAdapter: BluetoothAdapter? = null
     private var mBinding: ActivityTestBluetoothBinding? = null   //뷰바인딩
     private val binding get() = mBinding!!
     private var mScanning: Boolean = false
@@ -33,106 +34,88 @@ class test_Bluetooth : AppCompatActivity() {
         mBinding = ActivityTestBluetoothBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //권한
-        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        val intent: Intent
-        intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        if (mBluetoothAdapter.isEnabled) {
-            // 블루투스 관련 실행 진행
-        } else {
-            // 블루투스 활성화 하도록
-            startActivityForResult(intent, 1)
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (mBluetoothAdapter==null){
+            System.out.println("블루투스가 사용불가합니다.")
+            onDestroy()
         }
-        //
-        val bleOnOffBtn: ToggleButton = binding.bleOnOffBtn
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if(bluetoothAdapter!=null){             //블루투스 on/off초기설정
-            // Device doesn't support Bluetooth
-            if(bluetoothAdapter?.isEnabled==false){
-                bleOnOffBtn.isChecked = true
-            } else{
-                bleOnOffBtn.isChecked = false
-            }
+        var found_filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, found_filter)
+        var state_filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        registerReceiver(BlueCheck, state_filter)
+
+        active_bluetooth()      //블루투스 활성화
+
+        val bleOnOffBtn: ToggleButton = binding.bleOnOffBtn //블루투스 on/off초기설정
+        // Device doesn't support Bluetooth
+        if(check_bluetooth()==false){
+            bleOnOffBtn.isChecked = true
+        } else{
+            bleOnOffBtn.isChecked = false
         }
+
         bleOnOffBtn.setOnCheckedChangeListener { _, isChecked ->
             bluetoothOnOff()
         }
         //페어링된 기기 검색
-
-        var filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
-        //val pairedDevices = mBluetoothAdapter.bondedDevices
-
         binding.searchBtn.setOnClickListener(){
-            /*
-            if (pairedDevices.size > 0) {
-                System.out.println("검색결과")
-                for (device in pairedDevices) {
-                    Log.d(TAG, device.name)
-                    Log.d(TAG, device.address)
-                }
-            }
-            else{
-                System.out.println("검색결과없음")
-            }
-         */
-            //페어링된 기기 검색
-            val permission_list = arrayOf<String>(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-
-            ActivityCompat.requestPermissions(this, permission_list, 1)
-            val bleEnableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(bleEnableIntent, REQUEST_ENABLE_BT)
-            System.out.println(mBluetoothAdapter.startDiscovery())    //검색시작
+            System.out.println("검색전 종료 "+mBluetoothAdapter!!.cancelDiscovery())
+            System.out.println(mBluetoothAdapter!!.startDiscovery())    //검색시작
             System.out.println("디바이스 개수는" + arrayDevices.size)
 
         }
-        //페어링된 기기 검색 끝
-//        System.out.println("안드로이드 id")
-//        System.out.println(mBluetoothAdapter.name)   //찾았다
-//        mBluetoothAdapter.setName("mydevice")
-//        System.out.println(mBluetoothAdapter.name)
-//        System.out.println(BluetoothAdapter.EXTRA_LOCAL_NAME)
         binding.nexttest.setOnClickListener(){
             val intent = Intent(this, test_Bluetooth2::class.java)
             startActivity(intent)
         }
         binding.getname.setOnClickListener(){
-            Device_getname(mBluetoothAdapter)
+            Device_getname(mBluetoothAdapter!!)
         }
         binding.setname.setOnClickListener(){
-            Device_setname(mBluetoothAdapter,"jeahunkim")
+            Device_setname(mBluetoothAdapter!!,"jeahunkim")
         }
     }
+
+    fun check_bluetooth(): Boolean? {   //블루투스on/off상태 체크
+
+        System.out.println(mBluetoothAdapter?.isEnabled)
+        return mBluetoothAdapter?.isEnabled
+    }
     @SuppressLint("MissingPermission")
+    fun active_bluetooth(){ //블루투스활성화
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+    }
+    @SuppressLint("MissingPermission")  //블루투스명 가져오기
     fun Device_getname(mBluetoothAdapter: BluetoothAdapter){
         System.out.println(mBluetoothAdapter.name)
     }
-    @SuppressLint("MissingPermission")
+    @SuppressLint("MissingPermission")  //블루투스명 설정
     fun Device_setname(mBluetoothAdapter: BluetoothAdapter, name :String){
         mBluetoothAdapter.setName(name)
     }
-    private val receiver = object : BroadcastReceiver() {
 
+    private val receiver = object : BroadcastReceiver() {   //검색
         @SuppressLint("MissingPermission")
         override fun onReceive(context: Context, intent: Intent) {
-            val action: String = intent.action!!
+            val action: String = intent.action.toString()
             when(action) {
                 BluetoothDevice.ACTION_FOUND -> {
                     // Discovery has found a device. Get the BluetoothDevice
                     // object and its info from the Intent.
-                    val device: BluetoothDevice =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
-                    val deviceName = device.name
-                    val deviceHardwareAddress = device.address // MAC address
-                    System.out.println(deviceName)
-                    if (!arrayDevices.contains(device) && device.name!=null) {
-                        arrayDevices.add(device)
-                        System.out.println("arraydevice : "+ device.name)
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    if (device!=null){
+                        val deviceName = device.name
+                        val deviceHardwareAddress = device.address // MAC address
+                        System.out.println(deviceName)
+                        if (!arrayDevices.contains(device) && device.name!=null) {
+                            arrayDevices.add(device)
+                            System.out.println("arraydevice : "+ device.name)
+                        }
                     }
                     else{
-                        System.out.println("왜 널이지")
+                        System.out.println("arraydevice : null")
                     }
                 }
             }
@@ -141,31 +124,36 @@ class test_Bluetooth : AppCompatActivity() {
                 println(i.name)
             }
         }
+    }
+    private val BlueCheck = object : BroadcastReceiver() {  //블루투스 변경체크     완료
+        @SuppressLint("MissingPermission")
+        override fun onReceive(context: Context, intent: Intent) {
+            val state  = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,-1)
+            System.out.println("블루투스변경감지")
+            if (state == BluetoothAdapter.STATE_OFF) { // 블루투스 꺼져 있으면 블루투스 활성화
+                active_bluetooth()
+                Log.d("bluetoothAdapter","블루투스활성화")
+            }
 
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
         // Don't forget to unregister the ACTION_FOUND receiver.
         unregisterReceiver(receiver)
+        unregisterReceiver(BlueCheck)
     }
 
 
     @SuppressLint("MissingPermission")
     fun bluetoothOnOff(){
-        if (bluetoothAdapter == null) {     //블루투스를 지원하는지 확인
-            // Device doesn't support Bluetooth
-            Log.d("bluetoothAdapter","Device doesn't support Bluetooth")
-        }else{
-            if (bluetoothAdapter?.isEnabled == false) { // 블루투스 꺼져 있으면 블루투스 활성화
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                Log.d("bluetoothAdapter","블루투스활성화")
-            } else{ // 블루투스 켜져있으면 블루투스 비활성화
-                bluetoothAdapter?.disable()
-                Log.d("bluetoothAdapter","블루투스비활성화")
-            }
+        if (check_bluetooth() == false) { // 블루투스 꺼져 있으면 블루투스 활성화
+            active_bluetooth()
+            Log.d("bluetoothAdapter","블루투스활성화")
+        } else{ // 블루투스 켜져있으면 블루투스 비활성화
+            mBluetoothAdapter?.disable()
+            Log.d("bluetoothAdapter","블루투스비활성화")
         }
     }
     //검색
