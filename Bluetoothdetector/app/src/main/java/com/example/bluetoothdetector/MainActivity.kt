fe1d0fdf4d7281d5565f8728154035e7c3777002
YoungJin.Heo <YoungJin.Heo98@gmail.com>
2022-05-28 14:06:24 +0900

    ㅁ

--- Bluetoothdetector/app/src/main/java/com/example/bluetoothdetector/MainActivity.kt
+++ Bluetoothdetector/app/src/main/java/com/example/bluetoothdetector/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.bluetoothdetector
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bluetoothdetector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         binding.buttonBt.setOnClickListener(){
             val intent = Intent(this, test_Bluetooth::class.java)
             startActivity(intent)
@@ -21,8 +22,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, test_Ocr::class.java)
             startActivity(intent)
         }
-
-
     }
+
+
+
 
 }
